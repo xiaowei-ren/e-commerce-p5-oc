bca4c26dae5e2fd1d30069b28014ed0eab30528e
Xiaowei Ren <rxwgoodluck@hotmail.com>
2022-12-09 15:40:02 +0100

    ajouter le panier dans la page du produit

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,49 +1,63 @@
 
 //Récupération des packages depuis le fichier json
+//affiche les produits dans le panier
 fetch("http://localhost:3000/api/products")
   .then(res => res.json())
   .then(data => {
     console.log(data)
-    data.forEach(cart => addCart(cart))
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    for (i = 0; i < cart.length; i++) {
+      let cartProduct = cart[i]
+      let productDetail = data.find(p => p._id == cartProduct._id)
+        addCart(cart[i]);
+    }
   })
   .catch(err => console.error(err));
 
+  
 //Création des éléments
-function addCart (cart) {
+function addCart (product) {
   //création un nouvel élément article
    let article = document.createElement("article");
    article.classList.add("cart__item");
-   article.dataset.id = "cart_id";//?
-   article.dataset.color = "cart_colors";//?
+   article.dataset.id = "{product-ID}";  // ?
+   article.dataset.color = "{product-color}"; // ?
 
    let section = document.querySelector("#cart__items");
    section.appendChild(article);
 
- //création de la div img
+    //Insertion de l'image
    let imgContainer = document.createElement("div");
    imgContainer.classList.add("cart__item__img");
    article.appendChild(imgContainer);
 
    let img = document.createElement("img");
-   img.src = cart.imageUrl;
-   img.alt = cart.altTxt;
+   img.src = product.imageUrl;
+   img.alt = product.altTxt;
    imgContainer.appendChild(img);
 
   //crée de la div content
    let divContent = document.createElement("div");
    divContent.classList.add("cart__item__content");
    article.appendChild(divContent);
+
    //crée de la cart description <div class="cart__item__content__description">
    let divDescription = document.createElement("div");
    divDescription.classList.add("cart__item__content__description");
    divContent.appendChild(divDescription);
+
+   //Insertion du nom
    let h2 = document.createElement("h2");
-   h2.innerHTML = cart.name;
-   let p1 = document.createElement("p");
-   p1.innerHTML = cart.colors;
-   let p2 = document.createElement("p");
-   p2.innerHTML = cart.price + " €";
+   h2.innerHTML = product.name;
    divDescription.appendChild(h2);
+
+   // Insertion de la couleur
+   let p1 = document.createElement("p");// 
+   p1.innerText = product.color;
+
+  // Insertion du prix
+   let p2 = document.createElement("p");
+   p2.innerHTML = product.price + " €";//?
    divDescription.appendChild(p1);
    divDescription.appendChild(p2);
 
@@ -51,21 +65,34 @@ function addCart (cart) {
    let divSetting = document.createElement("div");
    divSetting.classList.add("cart__item__content__settings");
    divContent.appendChild(divSetting);
+
+   // Insertion de "Qté : "
    let divSetting_quant = document.createElement("div");
    divSetting_quant.classList.add("cart__item__content__settings__quantity");
    divSetting.appendChild(divSetting_quant);
    let divP = document.createElement("p");
    divP.innerHTML = "Qté : ";
+    
+   // Insertion de la quantité  ?
    let input = document.createElement("input");
+  
+   
    input.setAttribute("type", "number");
    input.classList.add("itemQuantity");
    input.setAttribute("name", "itemQuantity");
    input.setAttribute("min", "1");
    input.setAttribute("max", "100");
-   input.setAttribute("value", "42");
+   input.setAttribute("value", product.quantity);
    divSetting_quant.appendChild(divP);
    divSetting_quant.appendChild(input);
+
+   // mis a jour de la quantite dans le localStorage
+   input.addEventListener ("change", () => {
+     changeQuantity(product, parseInt(input.value))
+   })
    
+   
+  // Insertion de la partie de suppression
    let divSetting_delte = document.createElement("div");
    divSetting_delte.classList.add("cart__item__content__settings__delete");
    divSetting.appendChild(divSetting_delte);
@@ -74,5 +101,31 @@ function addCart (cart) {
    deletP.innerHTML = "Supprimer";
    divSetting_delte.appendChild(deletP);
 
+   deletP.addEventListener ("click", () => {
+     deleteItem(product)
+   })
+
 }
+
+function changeQuantity (product, quantity) {
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  let foundProduct = cart.find(p => p._id == product._id);
+  if (foundProduct != undefined) {
+    foundProduct.quantity = quantity;
+  }
+  localStorage.setItem('cart', JSON.stringify(cart))
+ }
+
+
+//supprime le produit et mis a jour les donnes dans le localStorage
+function deleteItem (product) {
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  cart = cart.filter(p => p._id != product._id && p.color != product.color);
+  localStorage.setItem('cart', JSON.stringify(cart))
+
+}
+
+
+
+
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,8 @@
-//Obtention des valeurs de chaîne (id)
-const urlSearchParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlSearchParams.entries());
+// Récuperer l'id du produit à afficher dans l'URL
+let urlQueryParams = new URLSearchParams(window.location.search);
+let params = Object.fromEntries(urlQueryParams.entries());
 let id = params.id;
+let productObject = {}
 
 fetch("http://localhost:3000/api/products")
   .then(res => res.json())
@@ -9,6 +10,7 @@ fetch("http://localhost:3000/api/products")
     console.log(data)
     data.forEach(content => {
       if(content._id == id) {
+        productObject = content
         addContent(content)
       }
     });
@@ -16,6 +18,7 @@ fetch("http://localhost:3000/api/products")
   .catch(error => console.log(error));
 
   //Crée des éléments
+  let select = document.getElementById("colors");
   function addContent (content) {
     let imgContainer = document.querySelector(".item__img"); 
     let img = document.createElement("img");
@@ -27,13 +30,71 @@ fetch("http://localhost:3000/api/products")
     document.getElementById("price").innerHTML = content.price;
     document.getElementById("description").innerHTML = content.description;
 
-    let select = document.getElementById("colors");
     content.colors.forEach(color => {
       let newOption = new Option(color.toLowerCase(), color.toLowerCase());
       select.appendChild(newOption);
     })
-
   }
+
+  //Ajouter le produit au pannier
+  let bouton = document.getElementById("addToCart");
+  let quantity = document.getElementById("quantity");
+
+  // Au clic, on ajoute un produit dans le panier
+  bouton.addEventListener("click", () => {
+
+    let cart = localStorage.getItem("cart") // Le panier
+    let quantityValue = parseInt(quantity.value); // La quantite choisie
+
+    if (select.value != '') {
+      // On verifie que la quantite choisie est bien comprise entre 1 a 100
+      if (quantityValue > 0 && quantityValue <= 100) {
+
+        // Les donnes a garder en memoire au clic
+      
+        var selection = {
+          "altTxt": productObject.altTxt,
+          "color": select.value,
+          "description": productObject.description,
+          "imageUrl": productObject.imageUrl,
+          "name": productObject.name,
+          "price": productObject.price,
+          "_id": id,
+          "quantity": quantityValue,
+        }
+
+        // Si le panier n'existe pas, 
+        // on cree un premier tableau avec la selection
+        if (cart == null) {
+          localStorage.setItem("cart", JSON.stringify([selection]));
+        } else {
+
+          // Si le panier existe,
+          // on garde en memoire la selection
+          let cartData = JSON.parse(cart)
+          // on verifie si la meme id et meme cloleur, on change la quantite
+          //si non, on ajoute un nouvel object dans le localStorage
+          let foundColor = cartData.find(p => (p._id == selection._id && p.color == selection.color));
+          if (foundColor != undefined) {
+            foundColor.quantity = foundColor.quantity + selection.quantity;
+          } else {
+            cartData.push(selection)
+          }
+          
+          localStorage.setItem("cart", JSON.stringify(cartData))
+          
+        }
+        console.log(JSON.parse(cart))
+        
+      // Sinon, on affiche une alerte
+      } else {
+        alert("Veuillez choisir entre 1 a 100 produits.")
+      }
+    } else {
+      alert("Veuillez choisir une couleur")
+    }
+    
+})
  
 
 
